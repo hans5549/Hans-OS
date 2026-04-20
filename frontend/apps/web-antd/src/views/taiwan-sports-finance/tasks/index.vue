c641bccefe133755d65316e8ce710b874f33b72a
hans-rog-pc <hanshuang19951204@gmail.com>
2026-04-20 13:25:42 +0800

    feat(tsf): 新增體育部財務「任務清單」空頁面與選單 migration

---
+++ frontend/apps/web-antd/src/views/taiwan-sports-finance/tasks/index.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import { Page } from '@vben/common-ui';
+
+defineOptions({ name: 'TsfTasksPage' });
+</script>
+
+<template>
+  <Page title="任務清單" description="體育部財務任務清單">
+    <div class="flex h-64 items-center justify-center text-gray-400">
+      即將推出
+    </div>
+  </Page>
+</template>
